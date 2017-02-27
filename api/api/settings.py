e05d33e817e1e40d1637360e994cdde6ddd2ba07
Simon Herrera <simonherrera.tech@gmail.com>
2017-02-27 08:54:08 -0600

    Debug to False

--- api/api/settings.py
+++ api/api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tlf9q=(r3lol5+ikkrjqau!gy6n_vbkr3)z7gddm&km@%ma143'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
